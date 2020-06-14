fbad8ac489fbc7fa0e48eb216c7f677a28a7c084
Yuchen Wang <yuchen_wng@hotmail.com>
2020-06-14 20:08:40 +0100

    Updates

---
+++ precache-manifest.9e7216c69447957895869e9c97e364c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1618d6a9a6a1246c084005a5c690d951",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "c4d3a56ede9a48c61282",
+    "url": "/blog/static/css/2.43403e34.chunk.css"
+  },
+  {
+    "revision": "559a292a0fbeb33d39c6",
+    "url": "/blog/static/css/main.d4d07486.chunk.css"
+  },
+  {
+    "revision": "c4d3a56ede9a48c61282",
+    "url": "/blog/static/js/2.f236583b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blog/static/js/2.f236583b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559a292a0fbeb33d39c6",
+    "url": "/blog/static/js/main.558ee473.chunk.js"
+  },
+  {
+    "revision": "f53fb3d3f912cb0e71ea",
+    "url": "/blog/static/js/runtime-main.6e81d047.js"
+  }
+]);
--- web-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-import "./App.css";
-import { Home } from "./from-builder";
-
-const App: FC = () => {
-	return <Home />;
-};
-
-export default App;
