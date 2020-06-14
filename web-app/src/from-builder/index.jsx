0e4c6c0825d6f46ae75ebc301df4521dff38ab7c
Yuchen Wang <yuchen_wng@hotmail.com>
2020-06-14 17:41:34 +0100

    House keeping and more tidying

--- web-app/src/from-builder/index.jsx
+++ web-app/src/from-builder/index.jsx
@@ -1,96 +1,52 @@
-/* eslint no-undef: 0 */
-/* eslint arrow-parens: 0 */
-import React from 'react';
-import { enquireScreen } from 'enquire-js';
+import React, { useState } from "react";
+import { enquireScreen } from "enquire-js";
 
-import Nav0 from './Nav0';
-import Banner2 from './Banner2';
-import Content3 from './Content3';
-import Content9 from './Content9';
-import Teams2 from './Teams2';
-import Footer0 from './Footer0';
+import Nav0 from "./Nav0";
+import Footer0 from "./Footer0";
 
-import {
-  Nav00DataSource,
-  Banner20DataSource,
-  Content30DataSource,
-  Content90DataSource,
-  Teams20DataSource,
-  Footer00DataSource,
-} from './data.source';
-import './scss/antMotionStyle.scss';
+import { Layout, Breadcrumb, Menu } from "antd";
 
+import { Nav00DataSource, Footer00DataSource } from "./data.source";
+import "./scss/antMotionStyle.scss";
+
+const { Content } = Layout;
+
+// eslint-disable-next-line no-unused-vars
 let isMobile;
 enquireScreen((b) => {
-  console.log(b);
-  isMobile = b;
+	isMobile = b;
 });
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMobile,      
-    };
-  }
+export const Home = () => {
+	const [localIsMobile, setIsMobile] = useState(false);
 
-  componentDidMount() {
-    // 适配手机屏幕;
-    enquireScreen((b) => {
-      this.setState({ isMobile: !!b });
-    });
-  }
+	enquireScreen((b) => {
+		setIsMobile(!!b);
+	});
 
-  render() {
-    const children = [
-      <Nav0
-        id="Nav0_0"
-        key="Nav0_0"
-        dataSource={Nav00DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Banner2
-        id="Banner2_0"
-        key="Banner2_0"
-        dataSource={Banner20DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content3
-        id="Content3_0"
-        key="Content3_0"
-        dataSource={Content30DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Content9
-        id="Content9_0"
-        key="Content9_0"
-        dataSource={Content90DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Teams2
-        id="Teams2_0"
-        key="Teams2_0"
-        dataSource={Teams20DataSource}
-        isMobile={this.state.isMobile}
-      />,
-      <Footer0
-        id="Footer0_0"
-        key="Footer0_0"
-        dataSource={Footer00DataSource}
-        isMobile={this.state.isMobile}
-      />,
-    ];
-    return (
-      <div
-        className="templates-wrapper"
-        ref={(d) => {
-          this.dom = d;
-        }}
-      >
-        
-        {children}
-        
-      </div>
-    );
-  }
-}
+	return (
+		<div className="templates-wrapper">
+			<Nav0
+				id="Nav0_0"
+				key="Nav0_0"
+				dataSource={Nav00DataSource}
+				isMobile={localIsMobile}
+			/>
+			<Content style={{ padding: "0 50px" }}>
+				<Breadcrumb style={{ margin: "16px 0" }}>
+					<Breadcrumb.Item>Home</Breadcrumb.Item>
+					<Breadcrumb.Item>List</Breadcrumb.Item>
+					<Breadcrumb.Item>App</Breadcrumb.Item>
+				</Breadcrumb>
+				<div className="site-layout-content">Content</div>
+			</Content>
+			,
+			<Footer0
+				id="Footer0_0"
+				key="Footer0_0"
+				dataSource={Footer00DataSource}
+				isMobile={localIsMobile}
+			/>
+		</div>
+	);
+};
