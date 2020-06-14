f1b875bdf801c6deffe5298cc9f7b8642360d456
Yuchen Wang <yuchen_wng@hotmail.com>
2020-06-14 20:45:13 +0100

    rename again

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import "./App.css";
-import { Home } from "./from-builder/home";
+import { Home } from "./from-builder/Home";
 
 const App: FC = () => {
 	return <Home />;
