8d99e61e473b5cdf191b387bddd3e9dd3fbfa8ef
Yuchen Wang <yuchen_wng@hotmail.com>
2020-07-05 14:21:09 +0100

    Revert "Barebones gatsby template"
    This reverts commit e0973d00190d885c0754899c35cdf5ef2cf2f243.

--- gatsby-web-app/src/components/layout.js
+++ gatsby-web-app/src/components/layout.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -24,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
--- gatsby-web-app/src/pages/index.tsx
+++ gatsby-web-app/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
@@ -12,7 +13,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      
+      <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
