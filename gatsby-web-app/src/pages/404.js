0e82af2357825af3f57b0a937c5cc2b6ef4fe312
Yuchen Wang <yuchen_wng@hotmail.com>
2020-07-05 13:05:49 +0100

    Revert "Migrate to antd"
    This reverts commit 588deeda09f9432a4dc3f8de6e8ee06399be49e3.

--- gatsby-web-app/src/pages/404.js
+++ gatsby-web-app/src/pages/404.js
@@ -1,20 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { BlogLayout } from "../components/BlogLayout"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
-    <BlogLayout>
-    
+    <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
       <h1>Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    
-    </BlogLayout>
+    </Layout>
   )
 }
 
--- gatsby-web-app/src/pages/index.tsx
+++ gatsby-web-app/src/pages/index.tsx
@@ -1,43 +1,46 @@
-import "antd/dist/antd.css"
-import React, { useState } from "react"
+import React from "react"
 import { Link, graphql } from "gatsby"
-import { BlogLayout } from "../components/BlogLayout"
-import { BlogContent } from "../components/BlogContent"
+
+import Bio from "../components/bio"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { rhythm } from "../utils/typography"
 
 const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
   return (
-    <>
-      <BlogLayout >
-        <BlogContent breadcrumb={["Landing Page"]}>
-        {
-          posts.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <article key={node.fields.slug}>
-                <header>
-                  <h3>
-                    <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>{node.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: node.frontmatter.description || node.excerpt,
-                    }}
-                  />
-                </section>
-              </article>
-            )
-          })
-        }
-        </BlogContent>
-        </BlogLayout>
-    </>
+    <Layout location={location} title={siteTitle}>
+      <SEO title="All posts" />
+      <Bio />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <article key={node.fields.slug}>
+            <header>
+              <h3
+                style={{
+                  marginBottom: rhythm(1 / 4),
+                }}
+              >
+                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
+                  {title}
+                </Link>
+              </h3>
+              <small>{node.frontmatter.date}</small>
+            </header>
+            <section>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: node.frontmatter.description || node.excerpt,
+                }}
+              />
+            </section>
+          </article>
+        )
+      })}
+    </Layout>
   )
 }
 
